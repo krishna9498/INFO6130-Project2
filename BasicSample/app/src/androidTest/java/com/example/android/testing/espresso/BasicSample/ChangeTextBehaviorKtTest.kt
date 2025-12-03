f10f9b711afdddf3c2a44aeb5088d8a3a8af7137
Yanja1 <krishnasubedi820@gmail.com>
2025-12-02 22:15:46 -0500

    All testing done on basicSample

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
@@ -1,84 +1,100 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.testing.espresso.BasicSample
 
-import androidx.test.ext.junit.rules.activityScenarioRule
-import android.app.Activity
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
-/**
- * The kotlin equivalent to the ChangeTextBehaviorTest, that
- * showcases simple view matchers and actions like [ViewMatchers.withId],
- * [ViewActions.click] and [ViewActions.typeText], and ActivityScenarioRule
- *
- *
- * Note that there is no need to tell Espresso that a view is in a different [Activity].
- */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ChangeTextBehaviorKtTest {
 
-    /**
-     * Use [ActivityScenarioRule] to create and launch the activity under test before each test,
-     * and close it after each test. This is a replacement for
-     * [androidx.test.rule.ActivityTestRule].
-     */
-    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
+    // Test 1: Validate correct strings in the TextView in the main activity
     @Test
-    fun changeText_sameActivity() {
-
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard())
-        onView(withId(R.id.changeTextBt)).perform(click())
-
-        // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)))
+    fun validateTextViewInMainActivity() {
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(R.string.hello_world)))
     }
 
+    // Test 2: Enter "123" and press Change Text button, and test the string
     @Test
-    fun changeText_newActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED),
-                closeSoftKeyboard())
-        onView(withId(R.id.activityChangeTextBtn)).perform(click())
-
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)))
+    fun enter123AndPressChangeTextButton() {
+        val inputText = "123"
+        onView(withId(R.id.editTextUserInput)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(inputText)
+        )
+        onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(inputText)))
     }
 
-    companion object {
+    // Test 3: Enter "123" and press Open Activity and Change Text button, and test the string in ShowTextActivity
+    @Test
+    fun enter123AndOpenActivityChangeTextButton() {
+        val inputText = "123"
+        onView(withId(R.id.editTextUserInput)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(inputText)
+        )
+        onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
+        onView(withId(R.id.show_text_view)).check(matches(withText(inputText)))
+    }
 
-        val STRING_TO_BE_TYPED = "Espresso"
+    // Test 4: Without entering anything and press Change Text button and test the string (empty/null)
+    @Test
+    fun emptyInputAndPressChangeTextButton() {
+        onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("")))
+    }
+
+    // Test 5: Without entering anything and press Open Activity and Change Text button, and test the string in ShowTextActivity
+    @Test
+    fun emptyInputAndOpenActivityChangeTextButton() {
+        onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
+        onView(withId(R.id.show_text_view)).check(matches(withText("")))
+    }
+
+    // Test 6: Enter "abcdef" and press Change Text button, and test the string
+    @Test
+    fun enterAbcdefAndPressChangeTextButton() {
+        val inputText = "abcdef"
+        onView(withId(R.id.editTextUserInput)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(inputText)
+        )
+        onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(inputText)))
+    }
+
+    // Test 7: Enter "abcdef" and press Open Activity and Change Text button, and test the string in ShowTextActivity
+    @Test
+    fun enterAbcdefAndOpenActivityChangeTextButton() {
+        val inputText = "abcdef"
+        onView(withId(R.id.editTextUserInput)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText(inputText)
+        )
+        onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
+        onView(withId(R.id.show_text_view)).check(matches(withText(inputText)))
+    }
+
+    // Test 8: Validate correct strings in the TextView in the ShowTextActivity
+    // This test needs to be run in ShowTextActivity context
+    @Test
+    fun validateTextViewInShowTextActivity() {
+        // First go to ShowTextActivity
+        onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
+        // Check the default/empty state
+        onView(withId(R.id.show_text_view)).check(matches(withText("")))
     }
 }
