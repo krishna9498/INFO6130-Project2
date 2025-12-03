0f09404578bf401e09a794b8f1aa629bca774e46
Yanja1 <krishnasubedi820@gmail.com>
2025-12-02 20:12:26 -0500

    Added first 5 EmailValidator test cases - Krishna

---
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -0,0 +1,33 @@
+package jlin2.examples.localtesting
+
+import jlin2.examples.localtesting.EmailValidator.Companion.isValidEmail
+import org.junit.Assert
+import org.junit.Test
+
+class EmailValidatorTest {
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        Assert.assertTrue(isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubdomain_ReturnsTrue() {
+        Assert.assertTrue(isValidEmail("123@abc.co.ca"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoDomain_ReturnsFalse() {
+        Assert.assertFalse(isValidEmail("123@abc"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleDots_ReturnsFalse() {
+        Assert.assertFalse(isValidEmail("123@abc..com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+        Assert.assertFalse(isValidEmail("@abc.com"))
+    }
+
+}
